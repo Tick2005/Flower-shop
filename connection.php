971f1c950e43fe27d8388ee5bbaa3e9fc9ad1b44
Tick2005 <phanvanduong1223456@gmail.com>
2025-05-06 16:06:57 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,18 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = ""; // hoặc password của bạn
-$dbname = "flower_db"; // tên CSDL của bạn
+$password = "";
+$dbname = "flower_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Kiểm tra kết nối
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+if (!isset($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
 ?>
