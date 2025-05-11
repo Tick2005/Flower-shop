e2dcd756d6773079c1a03c942fb1e8d2fabf190d
Tick2005 <phanvanduong1223456@gmail.com>
2025-05-11 20:08:06 +0700

    Update cart.php

--- cart.php
+++ cart.php
@@ -349,6 +349,7 @@ if (isset($_POST['logout'])) {
                         <a href="products.php?type=other">Other</a>
                     </div>
                 </div>
+                <a href="./customer.php" class="text-gray-700 hover:text-green-500">Home</a>
                 <div class="relative dropdown">
                     <a href="#" class="text-gray-700 hover:text-green-500 flex items-center space-x-2">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
