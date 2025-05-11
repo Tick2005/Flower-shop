38cf308cb688fa2906ada4796bd676df248aac7b
NguyenDucAnh <nguyenducanhwasabi@gmail.com>
2025-05-12 01:32:55 +0700

    Update customer_info.php

--- customer_info.php
+++ customer_info.php
@@ -214,6 +214,12 @@ if (isset($_POST['logout'])) {
         .dropdown {
             position: relative;
         }
+        .dropdown > a {
+            font-family: 'Arial', sans-serif;
+            color: #4b5563;
+            font-size: 1rem;
+            text-decoration: none;
+        }
         .dropdown-menu {
             visibility: hidden;
             opacity: 0;
@@ -385,17 +391,9 @@ if (isset($_POST['logout'])) {
                     </div>
                 </div>
             </div>
-            <div class="search-bar w-1/3">
-                <div class="relative">
-                    <input type="text" placeholder="Search flowers..." class="w-full pl- Antipasti, pr-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:border-green-500">
-                    <svg class="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                    </svg>
-                </div>
-            </div>
             <nav class="nav-links flex space-x-6 items-center">
                 <div class="relative dropdown">
-                    <a href="products.php" class="text-gray-7 Antipasti, hover:text-green-500">Products</a>
+                    <a href="products.php" class="text-gray-700 hover:text-green-500">Products</a>
                     <div class="dropdown-menu">
                         <a href="products.php?type=birthday">Birthday Flowers</a>
                         <a href="products.php?type=wedding">Wedding Flowers</a>
@@ -405,14 +403,14 @@ if (isset($_POST['logout'])) {
                         <a href="products.php?type=other">Other</a>
                     </div>
                 </div>
+                <a href="./customer.php" class="text-gray-700 hover:text-green-500">Home</a>
                 <div class="relative dropdown">
-                    <a href="#" class="text-gray-700 hover:text-green-500 flex items-center">
-                        <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <a href="#" class="text-gray-700 hover:text-green-500 flex items-center space-x-2">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                         </svg>
                         <span><?php echo htmlspecialchars($user_name); ?></span>
                     </a>
-                    <a href="./customer.php" class="text-gray-700 hover:text-green-500">Home</a>
                     <div class="dropdown-menu">
                         <a href="customer_info.php">My Account</a>
                         <form method="POST">
